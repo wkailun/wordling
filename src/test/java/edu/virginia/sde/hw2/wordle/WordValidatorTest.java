f96fe11a394cc9d071d0b8e3dd012f7cc1182555
cindyd02 <cindyydong02@gmail.com>
2023-09-23 23:50:41 -0400

    Wrote Test Cases for WordValidator
    Did two test cases each for the methods isValidWord(), isCorrectLength(), and isAllLetters()

--- src/test/java/edu/virginia/sde/hw2/wordle/WordValidatorTest.java
+++ src/test/java/edu/virginia/sde/hw2/wordle/WordValidatorTest.java
@@ -7,12 +7,42 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class WordValidatorTest {
     private static WordValidator wordValidator;
+
     @BeforeAll
     public static void initialize() {
         wordValidator = new WordValidator();
     }
+
     @Test
     public void isValidWord_false_tooLong() {
         assertFalse(wordValidator.isValidWord("DOGGIE"));
+    }
+
+
+    @Test
+    public void isValidWord_true_tooShort() {
+        assertTrue(wordValidator.isValidWord("earth"));
+    }
+
+    @Test
+    public void isCorrectLength_false() {
+        assertFalse(wordValidator.isCorrectLength("hey"));
+    }
+
+    @Test
+    public void isCorrectLength_false_whitespace() {
+        assertFalse(wordValidator.isCorrectLength("HOUSE "));
+
+    }
+
+    @Test
+    public void isAllLetters_false() {
+        assertTrue(wordValidator.isAllLetters("abcde"));
+    }
+
+    @Test
+    public void isAllLetters_mixed() {
+        assertTrue(wordValidator.isAllLetters("hOusE"));
+
     }
 }
