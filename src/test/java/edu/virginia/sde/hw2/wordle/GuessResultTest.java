e1e6264f3382d009b6ea44fd6e9e5e08338b6fde
wakilun <vxd4qa@virginia.edu>
2023-09-25 16:45:44 -0400

    Wrote test for getLetterResult to check for matches, exceptions, double/triple letters

--- src/test/java/edu/virginia/sde/hw2/wordle/GuessResultTest.java
+++ src/test/java/edu/virginia/sde/hw2/wordle/GuessResultTest.java
@@ -21,4 +21,50 @@ public class GuessResultTest {
 
         assertArrayEquals(new LetterResult[]{LetterResult.GREEN, LetterResult.GREEN, LetterResult.GREEN, LetterResult.GREEN, LetterResult.GREEN}, results);
     }
+
+    @Test
+    public void getLetterResult_allCorrect() {
+        GuessResult result = new GuessResult("basic", "basic");
+        assertArrayEquals(new LetterResult[]{LetterResult.GREEN, LetterResult.GREEN, LetterResult.GREEN, LetterResult.GREEN}, result.getLetterResults());
+    }
+
+    @Test
+    public void getLetterResult_allWrong() {
+        GuessResult result = new GuessResult("brain", "hello");
+        assertArrayEquals(new LetterResult[]{LetterResult.GRAY, LetterResult.GRAY, LetterResult.GRAY, LetterResult.GRAY, LetterResult.GRAY}, result.getLetterResults());
+    }
+
+    @Test
+    public void getLetterResult_someCorrect() {
+        GuessResult result = new GuessResult("brain", "basic");
+        assertArrayEquals(new LetterResult[]{LetterResult.GREEN, LetterResult.GRAY, LetterResult.YELLOW, LetterResult.GREEN, LetterResult.GRAY}, result.getLetterResults());
+    }
+    @Test
+    public void getLetterResult_nullInput(){
+        GuessResult guessResult = new GuessResult(null, "basic");
+        assertThrows(IllegalArgumentException.class, () -> guessResult.getLetterResults());
+    }
+
+    @Test
+    public void getLetterResult_incorrectLetterFormat() {
+        GuessResult wrongLength = new GuessResult("hundred", "hello");
+        assertThrows(IllegalArgumentException.class, () -> wrongLength.getLetterResults());
+
+        GuessResult invalidInputNumbers = new GuessResult("12345", "brain");
+        assertThrows(IllegalArgumentException.class, () -> invalidInputNumbers.getLetterResults());
+
+        GuessResult invalidInput = new GuessResult("**+=-", "brain");
+        assertThrows(IllegalArgumentException.class, () -> invalidInput.getLetterResults());
+    }
+    @Test
+    public void getLetterResult_doubleLetters() {
+        GuessResult guessResult = new GuessResult("dwell", "chill");
+        assertArrayEquals(new LetterResult[]{LetterResult.GRAY, LetterResult.GRAY, LetterResult.GRAY, LetterResult.GREEN, LetterResult.GREEN}, guessResult.getLetterResults());
+    }
+
+    @Test
+    public void getLetterResult_tripleLetters() {
+        GuessResult guessResult = new GuessResult("apple", "apppo");
+        assertArrayEquals(new LetterResult[]{LetterResult.GREEN, LetterResult.GREEN, LetterResult.GREEN, LetterResult.GRAY, LetterResult.GRAY}, guessResult.getLetterResults());
+    }
 }
